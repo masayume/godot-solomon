4751f732cf6f172c32b8abcfef22750e5ccfc026
masayume <masayume.blog@gmail.com>
2026-02-11 12:33:23 +0100

    blocks visible from spritesheet

--- scripts/block.gd
+++ scripts/block.gd
@@ -1,1 +1,14 @@
 extends StaticBody2D
+
+@export var tile_size: int = 64
+@export var variants: int = 6   # number of block images
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+func _ready():
+	randomize()
+	var tile_index = randi() % variants
+
+	var x = tile_index * tile_size
+	sprite.region_enabled = true
+	sprite.region_rect = Rect2(x, 0, tile_size, tile_size)
