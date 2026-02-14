1ed7057f72aed50d65aa609f24511bfdfff26051
masayume <masayume.blog@gmail.com>
2026-02-14 03:32:21 +0100

    fixed block distribution

--- scripts/block.gd
+++ scripts/block.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-@export var tile_size: int = 64
+@export var tile_size: int = 32
 @export var variants: int = 6   # number of block images
 
 @onready var sprite: Sprite2D = $Sprite2D
