93d05e84a3084c22f63067125d3799e31885d436
masayume <masayume.blog@gmail.com>
2026-03-10 14:11:58 +0100

    fixed right-only offset to create/destroy block

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -9,7 +9,7 @@ static func world_to_grid(world_pos: Vector2, x_off: float, y_off: float, tile_s
 	var local_x = world_pos.x - x_off
 	var local_y = world_pos.y + y_off
 
-	var grid_x = floor(local_x / tile_size)
+	var grid_x = floor((local_x - tile_size/2) / tile_size) 
 	var grid_y = -(floor(local_y / tile_size))
 
 	return Vector2i(grid_x, grid_y)
